94c3fa889617de91e71c9a6ffa3bf805a6e8d208
Carl Boettiger <cobettig@gmail.com>
2023-01-12 19:05:30 +0000

    move msy example to seperate file

---
+++ code/msy.py
@@ -0,0 +1,37 @@
+import gym_fishing
+import gym
+import pandas as pd
+import numpy as np
+
+env = gym.make("threeFishing-v2")
+
+df = []
+actions = np.linspace(0,.1,101)
+for action in actions:
+  episode_reward = 0
+  observation = env.reset()
+  for rep in range(10):
+    for t in range(200):
+      df.append(np.append([t, rep, action, episode_reward], observation))
+      observation, reward, terminated, info = env.step(action)
+      episode_reward += reward
+
+cols = ["t", "rep", "action", "reward", "sp1", "sp2", "sp3"]
+df = pd.DataFrame(df, columns = cols)
+df.to_csv("msy.csv")
+
+
+
+# identify action at associated with highest cumulative mean reward by last timestep
+df2 = ( df
+        .groupby(['t','action'], as_index=False)
+        .agg({'reward': 'mean',
+              'sp1': 'mean',
+              'sp2': 'mean',
+              'sp3': 'mean'})
+        .melt(id_vars=["t", "action", "reward"]) 
+        )
+best_action = df2.query("t==199")[df2.reward == max(df2.reward)].action.values[0]
+df3 = df.query("action == " + str(best_action))
+                                
+
--- code/train-agent.py
+++ code/train-agent.py
@@ -1,8 +1,7 @@
-from gym_fishing.envs import FishingCtsEnv
 from gym_fishing.envs import forageVVHcont
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.tune import register_env
-
+import os
 
 ## We could call env directly without this if only  our envs took a env_config dict argument
 register_env("fish-3sp", lambda config: forageVVHcont())
@@ -18,27 +17,27 @@ config = {
     "framework": "torch",
     "num_gpus": 1,
     "log_level": "ERROR",
-    "create_env_on_local_worker": True  # need this to restore env from checkpoint
+    "create_env_on_local_worker": True  # needed to restore env from checkpoint
 }
 
-trainer = PPOTrainer(config=config)
+agent = PPOTrainer(config=config)
 iterations = 250
-
-for _ in range(iterations):
-    trainer.train()
-    
-checkpoint = trainer.save("cache")
-
-
-# Evaluate a saved agent:
 checkpoint = ("cache/checkpoint_000{}".format(iterations))
-model = PPOTrainer(config=config)
-model.restore(checkpoint)
-model.evaluate()
 
-# simulate from the saved agent:
-env_config = model.evaluation_config.env_config
-env = model.env_creator(env_config)
+if not os.path.exists(checkpoint): # train only if no trained agent saved
+  for _ in range(iterations):
+      agent.train()
+  checkpoint = agent.save("cache")
+
+
+# Restore saved agent:
+agent = PPOTrainer(config=config)
+agent.restore(checkpoint)
+
+# agent.evaluate() # built-in method to evaluate agent on eval env
+
+# Initialize saved copy of eval environment:
+env = agent.env_creator(agent.evaluation_config.env_config)
 
 import pandas as pd
 import numpy as np
@@ -47,14 +46,16 @@ episode_reward = 0
 observation = env.reset()
 for rep in range(10):
   for t in range(200):
-    action = model.compute_single_action(observation)
+    action = agent.compute_single_action(observation)
     df.append(np.append([t, rep, action[0], episode_reward], observation))
     observation, reward, terminated, info = env.step(action)
     episode_reward += reward
 
 cols = ["t", "rep", "action", "reward", "sp1", "sp2", "sp3"]
 df = pd.DataFrame(df, columns = cols)
-#df.to_csv("msy.csv")
+df.to_csv("PPO.csv")
+
+
 
 from plotnine import ggplot, geom_point, aes, geom_line, facet_wrap, geom_path
 df2 = (df
@@ -76,42 +77,4 @@ df2 = (df
 
 
 
-
-
-df = []
-actions = np.linspace(0,.1,101)
-for action in actions:
-  episode_reward = 0
-  observation = env.reset()
-  for rep in range(10):
-    for t in range(200):
-      df.append(np.append([t, rep, action, episode_reward], observation))
-      observation, reward, terminated, info = env.step(action)
-      episode_reward += reward
-
-cols = ["t", "rep", "action", "reward", "sp1", "sp2", "sp3"]
-df = pd.DataFrame(df, columns = cols)
-df.to_csv("msy.csv")
-
-
-
-
-
-
-df2 = ( df
-        .groupby(['t','action'], as_index=False)
-        .agg({'reward': 'mean',
-              'sp1': 'mean',
-              'sp2': 'mean',
-              'sp3': 'mean'})
-        .melt(id_vars=["t", "action", "reward"]) 
-        ) 
-
-best_action = df2.query("t==199")[df2.reward == max(df2.reward)].action.values[0]
-
-q = "action == " + str(best_action)
-df3 = df.query(q)
-                                
-(ggplot(df3, aes("t", "value", group = interaction("rep", "variable"), color="variable")) +
- geom_line())
 
