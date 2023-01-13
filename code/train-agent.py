58c5aaf6127e2eca786bfd29df3f7e8cd95e9bd2
Carl Boettiger <cobettig@gmail.com>
2023-01-13 18:12:32 +0000

    plots of fixed action

--- code/train-agent.py
+++ code/train-agent.py
@@ -6,11 +6,11 @@ import pandas as pd
 import numpy as np
 
 ## We could call env directly without this if only  our envs took a env_config dict argument
-register_env("fish-3sp", lambda config: forageVVHcont())
+register_env("threeFishing-v2", lambda config: forageVVHcont())
 
 # Configure the algorithm.
 config = {
-    "env": "fish-3sp",
+    "env": "threeFishing-v2",
     "num_workers": 1,
     "num_envs_per_worker": 20,
     "resources": {
@@ -23,7 +23,7 @@ config = {
 }
 
 agent = PPOTrainer(config=config)
-iterations = 250
+iterations = 100
 checkpoint = ("cache/checkpoint_000{}".format(iterations))
 
 if not os.path.exists(checkpoint): # train only if no trained agent saved
@@ -49,7 +49,44 @@ for rep in range(10):
     df.append(np.append([t, rep, action[0], episode_reward], observation))
     observation, reward, terminated, info = env.step(action)
     episode_reward += reward
-
+    
+episode_reward
 cols = ["t", "rep", "action", "reward", "sp1", "sp2", "sp3"]
 df = pd.DataFrame(df, columns = cols)
-df.to_csv("data/PPO.csv.gz", index = False)
+df.to_csv("data/PPO" + str(iterations) + ".csv.gz", index = False)
+
+
+
+
+## Plots exploring agent action submanifolds
+from plotnine import ggplot, geom_point, aes, geom_line, facet_wrap, geom_path
+
+## agent action vs H
+dd = []
+for p in np.linspace(-1, 1, 101):
+  observation = np.append(observation[range(2)], p)
+  dd.append(np.append(p, agent.compute_single_action(observation) ))
+
+df2 = pd.DataFrame(dd, columns = ['H', 'action'])
+(ggplot(df2, aes("H", "action")) +geom_line())
+
+
+dd = []
+observation = env.reset()
+for p in np.linspace(-1, 1, 101):
+  observation = np.array([observation[0], p, observation[2]])
+  dd.append(np.append(p, agent.compute_single_action(observation) ))
+
+df2 = pd.DataFrame(dd, columns = ['V2', 'action'])
+(ggplot(df2, aes("V2", "action")) +geom_line())
+
+dd = []
+observation = env.reset()
+for p in np.linspace(-1, 1, 101):
+  observation = np.array([p, observation[1], observation[2]])
+  dd.append(np.append(p, agent.compute_single_action(observation) ))
+
+df2 = pd.DataFrame(dd, columns = ['V1', 'action'])
+(ggplot(df2, aes("V1", "action")) +geom_line())
+
+
