fced68dc25a7e4d843d7937d9e62340e13bef09a
Carl Boettiger <cobettig@gmail.com>
2023-01-14 06:36:53 +0000

    ray parallel

--- code/escapement.py
+++ code/escapement.py
@@ -2,17 +2,12 @@ import gym_fishing
 import gym
 import pandas as pd
 import numpy as np
+import ray
+from timebudget import timebudget
 
-# Note that state space is mapped to [-1,1]
-#
-# effort corresponding to constant escapement is
-# effort = max(1 - escapement / sp1_pop, 0)
-
-actions = np.linspace(0,1,101)
-env = gym.make("threeFishing-v2")
-env.reset()
-df = []
-for action in actions:
+@ray.remote
+def simulate(env, action):
+  df = []
   for rep in range(10):
     episode_reward = 0
     observation = env.reset()
@@ -21,12 +16,23 @@ for action in actions:
       sp1_pop = (observation[0] + 1 ) # natural state-space
       effort = np.max([1 - action / sp1_pop, 0])
       observation, reward, terminated, info = env.step(effort)
+      episode_reward += reward
       if terminated:
         break
-      episode_reward += reward
+  return(df)
+
+
+# parallelize over actions
+@timebudget
+def parallel(operation, env, input):
+  df = ray.get([operation.remote(env, i) for i in input])
+  return(df)
+
+
+env = gym.make("threeFishing-v2")
+actions = np.linspace(0,1,101)
+df = parallel(simulate, env, actions)
 
 cols = ["t", "rep", "action", "reward", "sp1", "sp2", "sp3"]
-df = pd.DataFrame(df, columns = cols)
-df.to_csv("data/escapement.csv.gz", index=False)
-
-
+df2 = pd.DataFrame(np.vstack(df), columns = cols)
+df2.to_csv("data/escapement.csv.gz", index=False)
--- code/msy.py
+++ code/msy.py
@@ -2,11 +2,12 @@ import gym_fishing
 import gym
 import pandas as pd
 import numpy as np
+import ray
+from timebudget import timebudget
 
-env = gym.make("threeFishing-v2")
-actions = np.linspace(0,.1,101)
-df = []
-for action in actions:
+@ray.remote
+def simulate(env, action):
+  df = []
   for rep in range(10):
     episode_reward = 0
     observation = env.reset()
@@ -14,26 +15,22 @@ for action in actions:
       df.append(np.append([t, rep, action, episode_reward], observation))
       observation, reward, terminated, info = env.step(action)
       episode_reward += reward
+  return(df)
+
+# parallelize over actions
+@timebudget
+def parallel(operation, env, input):
+  df = ray.get([operation.remote(env, i) for i in input])
+  return(df)
+
+
+env = gym.make("threeFishing-v2")
+actions = np.linspace(0,.1,101)
+df = parallel(simulate, env, actions)
 
 cols = ["t", "rep", "action", "reward", "sp1", "sp2", "sp3"]
-df = pd.DataFrame(df, columns = cols)
-df.to_csv("data/msy.csv.gz", index=False)
+df2 = pd.DataFrame(np.vstack(df), columns = cols)
+df2.to_csv("data/msy.csv.gz", index=False)
 
 
-
-
-
-# ignore me, we'll transform and plot in R
-# identify action at associated with highest cumulative mean reward by last timestep
-df2 = ( df
-        .groupby(['t','action'], as_index=False)
-        .agg({'reward': 'mean',
-              'sp1': 'mean',
-              'sp2': 'mean',
-              'sp3': 'mean'})
-        .melt(id_vars=["t", "action", "reward"]) 
-        )
-best_action = df2.query("t==199")[df2.reward == max(df2.reward)].action.values[0]
-df3 = df.query("action == " + str(best_action))
-                                
 
