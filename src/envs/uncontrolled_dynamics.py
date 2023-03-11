dcdd0c13c1d24958dc8f6f207bef7c5b1959dff9
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-03-11 11:32:57 -0800

    uncontrolled_dynamics works

--- src/envs/uncontrolled_dynamics.py
+++ src/envs/uncontrolled_dynamics.py
@@ -11,3 +11,14 @@ env = fish_tipping.three_sp(
 		config={"growth_fn": rockPaperScissors, "parameters": params},
 	)
 
+def timeEvolution(env, T=100):
+	path = np.empty((T, 4))
+	for t in range(T):
+		obs, rew, ter, aux, info = env.step(0)
+		pop = list(env.population())
+		path[t] = np.array([t, *pop])
+	path_df = pd.DataFrame(path, columns = ["t", "X", "Y", "Z"])
+	return path_df
+
+path_df = timeEvolution(env)
+print(path_df.head())
