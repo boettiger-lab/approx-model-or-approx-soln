a04a39566d7e75534976a42de0c0fae02110e447
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-06-15 16:19:26 -0700

    fractional_msy_script.py

--- src/msy_fns.py
+++ src/msy_fns.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import itertools
 
+""" Simulate """
+
 @ray.remote
 def msy_max_t_1fish(env, mortality, repetitions):
   x = []
@@ -101,6 +103,8 @@ def generate_msy_episodes_1fish(env, grid_nr=101, repetitions=100, only_max_time
     return df_max_times
   return df_max_times, df
 
+""" optimize """
+
 def find_msy_1fish(env, grid_nr=101, repetitions=100):
   df_max_times, df = generate_msy_episodes_1fish(env, grid_nr=grid_nr, repetitions=repetitions)
   tmp = (
@@ -129,6 +133,29 @@ def find_msy_2fish(env, grid_nr=51, repetitions=100):
   		(df.mortality_y == best.mortality_y.values[0])
   	]
 	)
+	
+""" fraction-of-msy const mortality simulation """
+
+def frac_msy_1fish(env, msy, *, fraction=0.8, repetitions = 100):
+  return msy_max_t_1fish(env, fraction * msy, repetitions=repetitions)
+
+def frac_msy_2fish(env, msy_x, msy_y, *, fraction=0.8, repetitions = 100):
+  return msy_max_t_1fish(env, fraction * msy_x, fraction * msy_y, repetitions=repetitions)
+
+# in practice I'll have to access the files I already made with the saved data
+def csv_to_frac_msy_1fish(env, fname, fraction=0.8, repetitions = 100):
+  df = pd.read_csv(fname)
+  msy = df.mortality[0]
+  return frac_msy_1fish(env, msy, fraction=fraction, repetitions=repetitions)
+
+def csv_to_frac_msy_2fish(env, fname, fraction=0.8, repetitions = 100):
+  df = pd.read_csv(fname)
+  msy_x = df.mortality_x[0]
+  msy_y = df.mortality_y[0]
+  return frac_msy_2fish(env, msy_x, msy_y, fraction=fraction, repetitions=repetitions)
+  
+
+""" misc """
 
 def msy_performances_1fish(env, grid_nr = 101, repetitions = 100):
   df_max_times = generate_msy_episodes_1fish(
@@ -140,3 +167,5 @@ def msy_performances_1fish(env, grid_nr = 101, repetitions = 100):
     .groupby(['act'], as_index=False)
     .agg({'reward': 'mean', 'reward_std':'std'})
   )
+  
+
