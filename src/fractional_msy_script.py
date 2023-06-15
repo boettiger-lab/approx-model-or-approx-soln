27dcea7b9a8b0bdf139a5e36aa6fdcfca558b6a7
Felipe Montealegre Mora <felimomouni@gmail.com>
2023-06-15 16:38:06 -0700

    preliminary-finished frac_msy script

--- src/fractional_msy_script.py
+++ src/fractional_msy_script.py
@@ -3,30 +3,49 @@ import seaborn as sns
 from plotnine import ggplot, aes, geom_bar, geom_point, geom_line
 import os
 
-from envs import two_three_fishing, singleSp
+from envs import two_three_fishing, oneSpecies
 from train_fns import create_env
 from parameters import parameters, parameters_oneSp
 from msy_fns import csv_to_frac_msy_1fish, csv_to_frac_msy_2fish
 
-#globals
-1sp1fish = ["data", "results_data", "ONESP", "high_beta", "msy_100.csv.xz"]
-3sp1fish = ["data", "results_data", "1FISHERY", "DEFAULT", "msy_100.csv.xz"]
-3sp2fish = ["data", "results_data", "2FISHERY", "DEFAULT", "msy_100.csv.xz"]
-3sp2fish_timevar = ["data", "results_data", "2FISHERY", "RXDRIFT", "msy_100.csv.xz"]
-
-env_class = two_three_fishing
+1sp1fish = ["data", "results_data", "ONESP", "high_beta"]
+3sp1fish = ["data", "results_data", "1FISHERY", "DEFAULT"]
+3sp2fish = ["data", "results_data", "2FISHERY", "DEFAULT"]
+3sp2fish_timevar = ["data", "results_data", "2FISHERY", "RXDRIFT"]
 
 parameter_obj_3sp = parameters()
 parameter_obj_1sp = parameters_oneSp()
 
+# choosing env to evaluate / corresponding data location
+scenario = 1sp1fish
+env_class = oneSpecies.singleSp
+fname = os.path.join(*scenario, "msy_100.csv.xz")
+DATACODE = "ONESP"
+
 env = create_env(
-  env_class = , 
+  env_class = env_class, 
   parameter_obj, 
-  datacode = "DEFAULT", 
-  fluctuating=True, 
+  datacode = DATACODE, 
+  fluctuating=False, 
   training=True
 )
 
-fname = os.path.join(*1sp1fish)
 
-msy_frac_df = csv_to_frac_msy_1fish()
+msy_frac_df, _ = csv_to_frac_msy_1fish(
+  env, fname, fraction=0.8, repetitions=100
+)
+del _
+
+msy_frac_df.to_csv(
+  os.path.join(*scenario, "frac_0-8_msy.csv.xz")
+)
+
+# plots
+
+rew_hist = (
+  ggplot(data=msy_frac_df, mapping=aes(x='rep',weight='reward')) 
+  +geom_bar()
+)
+msy_frac_df.save(
+  os.path.join(*scenario, "frac_0-8_msy_rewards.png")
+)
